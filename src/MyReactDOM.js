620f564b22ac7c01a3c3f60761ec172582a2081a
chengtianqing <chengtianqing@theduapp.com>
2020-05-27 09:27:15 +0800

    feat:添加hostconfig接口说明

--- src/MyReactDOM.js
+++ src/MyReactDOM.js
@@ -1,7 +1,14 @@
 import ReactReconciler from 'react-reconciler';
 
 const reconciler = ReactReconciler({
+
+  /**
+   * 开启功能
+   */
+  // 开启子节点修改（不开启无法更新）
   supportsMutation: true,
+  // // 开启hydrate，ssr
+  // supportsHydration: false,
 
   createInstance(
       type,
@@ -27,6 +34,11 @@ const reconciler = ReactReconciler({
   ) {
     return document.createTextNode(text);
   },
+  // 是否需要创建子文本节点，返回true则不创建，特例dangerouslySetInnerHTML
+  shouldSetTextContent() {
+    return false
+  },
+
   appendChildToContainer(container, child) {
     container.appendChild(child);
   },
@@ -36,7 +48,7 @@ const reconciler = ReactReconciler({
   appendInitialChild(parent, child) {
     parent.appendChild(child);
   },
-  
+
   removeChildFromContainer(container, child) {},
   removeChild(parent, child) {},
   insertInContainerBefore(container, child, beforeChild) {},
@@ -60,12 +72,17 @@ const reconciler = ReactReconciler({
       finishedWork,
   ) {},
 
+  /**
+   * 上下文
+   */
+  // 获取根容器的上下文信息, 只在根节点调用一次
   getRootHostContext() {},
+  // 获取子节点的上下文信息, 每遍历一个节点都会调用一次
   getChildHostContext() {},
+
   prepareForCommit() {},
   resetAfterCommit() {},
   finalizeInitialChildren() {},
-  shouldSetTextContent() {},
 });
 
 const MyReactDOM = {
--- src/ReactDomMini.js
+++ src/ReactDomMini.js
@@ -1,30 +1,58 @@
 import ReactReconciler from 'react-reconciler';
 
 const reconciler = ReactReconciler({
+    
   supportsMutation: true,
 
+  /**
+   * 上下文信息
+   */
+  // 根容器上下文，只在根节点调用一次
+  getRootHostContext() {},
+  // 子节点上下文，每遍历一个节点都会调用一次
+  getChildHostContext() {},
+  
+  /**
+   * 创建节点实例
+   */
+  // 普通节点
   createInstance(
       type,
       props,
       rootContainerInstance,
       hostContext,
       internalInstanceHandle,
-  ) {
-      console.log(type, props);
-  },
+  ) {},
+  // 文本节点
   createTextInstance(
       text,
       rootContainerInstance,
       hostContext,
       internalInstanceHandle,
   ) {},
-  appendChildToContainer(container, child) {},
-  appendChild(parent, child) {},
+  // 是否需要处理子文本节点，不想处理返回true(当前dom内部消化)
+  shouldSetTextContent() {},
+
+  /**
+   * 节点树的构建
+   */
+  // 初始化节点（初始化阶段）
   appendInitialChild(parent, child) {},
-  removeChildFromContainer(container, child) {},
-  removeChild(parent, child) {},
-  insertInContainerBefore(container, child, beforeChild) {},
+  // 添加节点
+  appendChild(parent, child) {},
+  // 添加子节点到容器节点
+  appendChildToContainer(container, child) {},
+  // 插入子节点
   insertBefore(container, child, beforeChild) {},
+  // 插入子节点到容器节点
+  insertInContainerBefore(container, child, beforeChild) {},
+  // 删除子节点
+  removeChild(parent, child) {},
+  // 从容器节点中删除子节点
+  removeChildFromContainer(container, child) {},
+  
+ 
+  
 
   prepareUpdate(
       instance,
@@ -44,12 +72,10 @@ const reconciler = ReactReconciler({
       finishedWork,
   ) {},
   
-  getRootHostContext() {},
-  getChildHostContext() {},
+  
   prepareForCommit() {},
   resetAfterCommit() {},
   finalizeInitialChildren() {},
-  shouldSetTextContent() {},
 });
 
 const MyReactDOM = {
